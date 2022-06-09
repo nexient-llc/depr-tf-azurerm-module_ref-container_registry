51e91479b852a794b05fd1bb4cc52041f5c599d8
Debasish Sahoo <dsahoo@nexient.com>
2022-06-08 23:41:47 -0400

    Modified the outputs

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,27 @@ output "acr_name" {
     value = local.container_registry_name
 }
 
-output "container_registry" {
-    value = module.container_registry.container_registry
+output "acr_id" {
+    value = module.container_registry.container_registry.id
+    sensitive = true
+}
+
+output "acr_login_server" {
+    value = module.container_registry.container_registry.login_server
+    sensitive = true
+}
+
+output "acr_admin_username" {
+    value = module.container_registry.container_registry.admin_username
+    sensitive = true
+}
+
+output "acr_login_password" {
+    value = module.container_registry.container_registry.admin_password
+    sensitive = true
+}
+
+output "public_network_access_enabled" {
+    value = module.container_registry.container_registry.public_network_access_enabled
     sensitive = true
 }
