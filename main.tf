ee4049a361c2731cf8fd8322dafb626a4d49d002
Debasish Sahoo <dsahoo@nexient.com>
2022-06-16 21:23:05 -0400

    Added the missing variables

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 module "resource_name" {
   source    = "git::git@github.com:nexient-llc/tf-module-resource_name.git?ref=main"
 
-  for_each = local.resource_types
+  for_each = var.resource_types
 
-  logical_product_name = var.logical_product_name
-  region               = var.resource_group.location
-  class_env            = var.class_env
-  cloud_resource_type  = each.value.type
-  instance_env         = var.instance_env
-  instance_resource    = var.instance_resource
-  maximum_length       = each.value.maximum_length
+  logical_product_name  = var.logical_product_name
+  region                = var.resource_group.location
+  class_env             = var.class_env
+  cloud_resource_type   = each.value.type
+  instance_env          = var.instance_env
+  instance_resource     = var.instance_resource
+  maximum_length        = each.value.maximum_length
+  use_azure_region_abbr = var.use_azure_region_abbr
 }
 
 module "container_registry" {
